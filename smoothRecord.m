2288fd9ceecb7032bb43c4a6b363eba37de7a4eb
Xinlong Du <du.xinl@husky.neu.edu>
2020-12-09 01:49:15 -0500

    Smoothed a suite of wind records

--- smoothRecord.m
+++ smoothRecord.m
@@ -1,13 +1,11 @@
-clear;close all;clc;
-%% load hurrican wind speed and direction records
-uSpdDir=load('.\windRecords\NYRSimHurV4_NE7_NYR2_Sim1_50km.txt');
+function smoothRecord(uSpdDir,filename)
+%% stairs plot
 uSpd=uSpdDir(:,1);
 uDir=uSpdDir(:,2);
-%stairs plot
 time=0:60:9*60;
 time=time';
 uSpd=[uSpd;0];
-uDir=[uDir;0];
+uDir=[uDir;uDir(end)];
 figure
 yyaxis left
 stairs(time,uSpd)
@@ -19,23 +17,43 @@ stairs(time,uDir)
 ylabel('wind direction (rad)')
 ylim([0 2*pi])
 %% smooth use Logistic function
+k=1;
 x0=5;
 x=0.5:1:9.5;
 x=x';
-L=uSpd(1);
-k=1;
-y=logistic(x,L,k,x0);
+Lspd=uSpd(1);
+Ldir=0;
+ySpd=logistic(x,Lspd,k,x0);
+yDir=logistic(x,Ldir,k,x0);
 timeIn=floor(x);
-uSpdIn=y;
+uSpdIn=ySpd;
+uDirIn=yDir+uDir(1);
 for i=1:9
-    L=uSpd(i+1)-uSpd(i);
-    y=logistic(x,L,k,x0)+uSpd(i);
+    Lspd=uSpd(i+1)-uSpd(i);
+    Ldir=uDir(i+1)-uDir(i);
+    ySpd=logistic(x,Lspd,k,x0)+uSpd(i);
+    yDir=logistic(x,Ldir,k,x0)+uDir(i);
     timeIn=[timeIn;time(i:i+1)+i*10];
     timeIn=[timeIn;timeIn(end)+(1:9)'];
-    uSpdIn=[uSpdIn;uSpd(i);y];
+    uSpdIn=[uSpdIn;uSpd(i);ySpd];
+    uDirIn=[uDirIn;uDir(i);yDir];
 end
 timeIn=[timeIn;640];
 uSpdIn=[uSpdIn;0];
+uDirIn=[uDirIn;uDirIn(end)];
+%% plot and save
 figure
-stairs(timeIn,uSpdIn,'k.-')
+yyaxis left
+stairs(timeIn,uSpdIn)
+xlabel('time (min)')
+ylabel('wind speed (m/s)')
 ylim([0 70])
+yyaxis right
+stairs(timeIn,uDirIn)
+ylabel('wind direction (rad)')
+ylim([0 2*pi])
+fileID=fopen(fullfile('.\smoothRecords',filename),'w');
+for i = 1:length(timeIn)
+    fprintf(fileID,'%3i %7.4f %7.4f\n',timeIn(i),uSpdIn(i),uDirIn(i));
+end
+fclose(fileID);

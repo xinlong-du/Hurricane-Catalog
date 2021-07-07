f81ac26f8ce4b19bff66c539d4034b25b2299c18
Xinlong Du <du.xinl@husky.neu.edu>
2021-07-07 00:21:40 -0400

    Linear interpolation of 7 parameters

---
+++ windRecordlinInterp.m
@@ -0,0 +1,76 @@
+clear;clc;
+NYRSimHurV4_NE1=load('.\syntheticHurricanes\NYRSimHurV4_NE1.mat');
+%% hurricane NYRSimHurV4_NE1_NYR1_Sim1
+NYR=1;
+Sim=1;
+hurr=NYRSimHurV4_NE1.NYRSimHur(NYR).SimHur(Sim);
+hurrName=sprintf('NYRSimHurV4_NE1_NYR%1i_Sim%1i',NYR,Sim);
+refEye=16;
+dLati=-0.9;
+dLong=-1.12;
+duration=9;
+%% plot world map
+latlim = [10 70];
+lonlim = [-110 10];
+figure
+worldmap(latlim,lonlim)
+load coastlines
+plotm(coastlat,coastlon)
+geoshow(coastlat,coastlon,'color','k')
+hold on
+%% plot hurricane track
+lati1=hurr.Lat(2:end-1);
+long1=hurr.Lon(2:end-1);
+plotm(lati1,long1,'or')
+%% exact wind speed records at a location
+latiLoc=lati1(refEye)+dLati;
+longLoc=long1(refEye)+dLong;
+arclenLoc = distance(lati1(refEye),long1(refEye),latiLoc,longLoc);
+rLoc=deg2km(arclenLoc);
+%% linear interpolation of 7 parameters into 10 min interval
+theta=hurr.HeadDir(2:end-1);
+Vt=hurr.Vt_mps(2:end-1);
+B=hurr.B(2:end-1);
+dP=hurr.dP(2:end-1);
+Rmax=hurr.Rmax(2:end-1);
+rho=1.0;
+time=(0:36:49*36)';
+timeIn=(0:1:49*36)';
+
+lati1In=interp1q(time,lati1,timeIn);
+long1In=interp1q(time,long1,timeIn);
+thetaIn=interp1q(time,theta,timeIn);
+VtIn=interp1q(time,Vt,timeIn);
+BIn=interp1q(time,B,timeIn);
+dPIn=interp1q(time,dP,timeIn);
+RmaxIn=interp1q(time,Rmax,timeIn);
+%% generate wind record for a location
+V=zeros(length(B)-1,1);
+dir=zeros(length(B)-1,1);
+t=0:length(B)-2;
+for i=1:length(B)-1
+    [arclen,az] = distance(lati1(i),long1(i),latiLoc,longLoc);
+    alpha=deg2rad(az-theta(i+1)); %i+1 consider NaN for the first datum, same for Vt(i+1) 
+    r=deg2km(arclen);
+    f=2*7.2921*10^(-5)*sin(deg2rad(latiLoc));
+    V(i)=0.5*(Vt(i+1)*sin(alpha)-f*r)+sqrt(0.25*(Vt(i+1)*sin(alpha)-f*r)^2+...
+            B(i)*dP(i)*100/rho*(Rmax(i)/r)^B(i)*exp(-(Rmax(i)/r)^B(i)));
+    dir(i)=deg2rad(az)-pi/2;
+    if dir(i)>2*pi
+        dir(i)=dir(i)-2*pi;
+    elseif dir(i)<0
+        dir(i)=dir(i)+2*pi;
+    end
+end
+%% plot wind records
+recTime=refEye-(duration-1)/2:refEye+(duration-1)/2;
+figure
+yyaxis left
+plot(6*t(recTime),V(recTime))
+xlabel('time (h)')
+ylabel('wind speed (m/s)')
+ylim([0 70])
+yyaxis right
+plot(6*t(recTime),dir(recTime))
+ylabel('wind direction (rad)')
+ylim([0 2*pi])

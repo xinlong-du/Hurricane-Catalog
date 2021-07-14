b83d755d04ecc30ff2a3c725d82cb9509873fe9d
Xinlong Du <du.xinl@husky.neu.edu>
2021-07-14 18:17:25 -0400

    Linear interpolation of 7 parameters

--- windRecordlinInterp.m
+++ windRecordlinInterp.m
@@ -27,7 +27,7 @@ latiLoc=lati1(refEye)+dLati;
 longLoc=long1(refEye)+dLong;
 arclenLoc = distance(lati1(refEye),long1(refEye),latiLoc,longLoc);
 rLoc=deg2km(arclenLoc);
-%% linear interpolation of 7 parameters into 10 min interval
+%% 7 parameters into 6-hour interval
 theta=hurr.HeadDir(2:end-1);
 Vt=hurr.Vt_mps(2:end-1);
 B=hurr.B(2:end-1);
@@ -36,7 +36,6 @@ Rmax=hurr.Rmax(2:end-1);
 rho=1.0;
 time=(0:36:49*36)';
 timeIn=(0:1:49*36)';
-
 lati1In=interp1q(time,lati1,timeIn);
 long1In=interp1q(time,long1,timeIn);
 thetaIn=interp1q(time,theta,timeIn);
@@ -44,33 +43,44 @@ VtIn=interp1q(time,Vt,timeIn);
 BIn=interp1q(time,B,timeIn);
 dPIn=interp1q(time,dP,timeIn);
 RmaxIn=interp1q(time,Rmax,timeIn);
-%% generate wind record for a location
-V=zeros(length(B)-1,1);
-dir=zeros(length(B)-1,1);
-t=0:length(B)-2;
-for i=1:length(B)-1
-    [arclen,az] = distance(lati1(i),long1(i),latiLoc,longLoc);
-    alpha=deg2rad(az-theta(i+1)); %i+1 consider NaN for the first datum, same for Vt(i+1) 
+%% generate interpolated wind record for a location
+VIn=zeros(length(BIn)-1,1);
+dirIn=zeros(length(BIn)-1,1);
+tIn=0:length(BIn)-2;
+for i=1:length(BIn)-1
+    [arclen,az] = distance(lati1In(i),long1In(i),latiLoc,longLoc);
+    alpha=deg2rad(az-thetaIn(i+1)); %i+1 consider NaN for the first datum, same for Vt(i+1) 
     r=deg2km(arclen);
     f=2*7.2921*10^(-5)*sin(deg2rad(latiLoc));
-    V(i)=0.5*(Vt(i+1)*sin(alpha)-f*r)+sqrt(0.25*(Vt(i+1)*sin(alpha)-f*r)^2+...
-            B(i)*dP(i)*100/rho*(Rmax(i)/r)^B(i)*exp(-(Rmax(i)/r)^B(i)));
-    dir(i)=deg2rad(az)-pi/2;
-    if dir(i)>2*pi
-        dir(i)=dir(i)-2*pi;
-    elseif dir(i)<0
-        dir(i)=dir(i)+2*pi;
+    VIn(i)=0.5*(VtIn(i+1)*sin(alpha)-f*r)+sqrt(0.25*(VtIn(i+1)*sin(alpha)-f*r)^2+...
+            BIn(i)*dPIn(i)*100/rho*(RmaxIn(i)/r)^BIn(i)*exp(-(RmaxIn(i)/r)^BIn(i)));
+    dirIn(i)=deg2rad(az)-pi/2;
+    if dirIn(i)>2*pi
+        dirIn(i)=dirIn(i)-2*pi;
+    elseif dirIn(i)<0
+        dirIn(i)=dirIn(i)+2*pi;
     end
 end
-%% plot wind records
-recTime=refEye-(duration-1)/2:refEye+(duration-1)/2;
+%% plot interpolated wind records
 figure
 yyaxis left
-plot(6*t(recTime),V(recTime))
-xlabel('time (h)')
+plot(10*tIn,VIn)
+xlabel('time (min)')
 ylabel('wind speed (m/s)')
 ylim([0 70])
 yyaxis right
-plot(6*t(recTime),dir(recTime))
+plot(10*tIn,dirIn)
 ylabel('wind direction (rad)')
 ylim([0 2*pi])
+
+% recTime=(refEye-(duration-1)/2)*36:(refEye+(duration-1)/2)*36;
+% figure
+% yyaxis left
+% plot(10*tIn(recTime),VIn(recTime))
+% xlabel('time (min)')
+% ylabel('wind speed (m/s)')
+% ylim([0 70])
+% yyaxis right
+% plot(10*tIn(recTime),dirIn(recTime))
+% ylabel('wind direction (rad)')
+% ylim([0 2*pi])

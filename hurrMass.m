299d12263b11bf547bce2c005b5687fa8372afe0
Xinlong Du <du.xinl@husky.neu.edu>
2021-07-16 02:20:39 -0400

    Apply the wind speed threshold
    Plot the wind duration histogram

--- hurrMass.m
+++ hurrMass.m
@@ -4,7 +4,7 @@ hurr10000=load('.\syntheticHurricanes\NYRSimHurV4_NE1.mat');
 nHurr=0;
 NYR=[];
 SIM=[];
-for i=1:200
+for i=1:10000
     N=length(hurr10000.NYRSimHur(i).SimHur);
     for j=1:N
         latc=hurr10000.NYRSimHur(i).SimHur(j).Lat;
@@ -65,3 +65,28 @@ for i=1:nSeleHurr
     ylabel('wind direction (rad)')
     ylim([0 2*pi])
 end
+%% find the wind > threshold
+duration=zeros(nSeleHurr,1);
+for i=1:nSeleHurr
+    plotWind=seleHurrAll{i};
+    idx=find(plotWind.VIn>threshold);
+    duration(i)=10*plotWind.tIn(idx(end))-10*plotWind.tIn(idx(1));
+    figure
+    yyaxis left
+    plot(10*plotWind.tIn(idx(1):idx(end)),plotWind.VIn(idx(1):idx(end)))
+    xlabel('time (min)')
+    ylabel('wind speed (m/s)')
+    ylim([0 70])
+    yyaxis right
+    plot(10*plotWind.tIn(idx(1):idx(end)),plotWind.dirIn(idx(1):idx(end)))
+    ylabel('wind direction (rad)')
+    ylim([0 2*pi])
+end
+idx=find(duration<1000 & duration>0);
+duration=duration(idx);
+duration=duration/60;
+meanDura=mean(duration);
+figure
+histogram(duration,20,'Normalization','probability')
+xlabel('Duration (h)')
+ylabel('Probability')

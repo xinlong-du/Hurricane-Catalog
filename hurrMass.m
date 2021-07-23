2d97e255071d33a6d8dfca3631f1d9a3637c373f
Xinlong Du <du.xinl@husky.neu.edu>
2021-07-23 19:59:05 -0400

    Find hurricanes within 250 km of the location of interest

--- hurrMass.m
+++ hurrMass.m
@@ -1,15 +1,29 @@
 clear;clc;
-%% find hurricanes pass Massachusetts
+%% define the location of interest
+latLoc=42.3601; %Boston
+lonLoc=-71.0589;
+% latiLoc=28.5383; %Orlando
+% longLoc=-81.3792;
+rad = 250; %radius, consider hurricanes within 250 km of the location
+[latC,lonC] = scircle1(latLoc,lonLoc,km2deg(rad));
+massachusetts = shaperead('usastatehi',...
+   'UseGeoCoords',true,...
+   'Selector',{@(name) strcmpi(name,'Massachusetts'),'Name'});
+usamap('massachusetts')
+geoshow(massachusetts,'FaceColor','none')
+plotm(latLoc,lonLoc,'r*')
+plotm(latC,lonC,'r')
+%% find hurricanes within 250 km of the location
 hurr10000=load('.\syntheticHurricanes\NYRSimHurV4_NE1.mat');
 nHurr=0;
 NYR=[];
 SIM=[];
-for i=1:10000
+for i=1:100
     N=length(hurr10000.NYRSimHur(i).SimHur);
     for j=1:N
-        latc=hurr10000.NYRSimHur(i).SimHur(j).Lat;
-        lonc=hurr10000.NYRSimHur(i).SimHur(j).Lon;
-        [loni,lati]=checkHurrMass(lonc,latc);
+        latHurrj=hurr10000.NYRSimHur(i).SimHur(j).Lat;
+        lonHurrj=hurr10000.NYRSimHur(i).SimHur(j).Lon;
+        [loni,lati]=polyxpoly(lonC,latC,lonHurrj,latHurrj);
         if ~isempty(loni)
             nHurr=nHurr+1;
             NYR=[NYR;i];
@@ -18,29 +32,26 @@ for i=1:10000
     end
 end
 %% plot the selected hurricanes
-    figure
-    latlim = [10 70];
-    lonlim = [-110 10];
-    worldmap(latlim,lonlim)
-    load coastlines
-    plotm(coastlat,coastlon)
-    geoshow(coastlat,coastlon,'color','k')
-    hold on
+figure
+latlim = [10 70];
+lonlim = [-110 10];
+worldmap(latlim,lonlim)
+load coastlines
+plotm(coastlat,coastlon)
+geoshow(coastlat,coastlon,'color','k')
+hold on    
 for i=1:nHurr
-    lati1=hurr10000.NYRSimHur(NYR(i)).SimHur(SIM(i)).Lat;
-    long1=hurr10000.NYRSimHur(NYR(i)).SimHur(SIM(i)).Lon;
-    plotm(lati1,long1,'r')
+    latHurr=hurr10000.NYRSimHur(NYR(i)).SimHur(SIM(i)).Lat;
+    lonHurr=hurr10000.NYRSimHur(NYR(i)).SimHur(SIM(i)).Lon;
+    plotm(latHurr,lonHurr,'r')
 end
+plotm(latC,lonC,'b')
 %% calculate wind speed for a location
-latiLoc=42.3601; %Boston
-longLoc=-71.0589;
 threshold=41.0;
-% latiLoc=28.5383; %Orlando
-% longLoc=-81.3792;
 nSeleHurr=0;
 for i=1:nHurr
     hurr=hurr10000.NYRSimHur(NYR(i)).SimHur(SIM(i));
-    [maxV,maxVIn,minDist,tIn,VIn,dirIn]=windRecordlinInterp(hurr,latiLoc,longLoc);
+    [maxV,maxVIn,minDist,tIn,VIn,dirIn]=windRecordlinInterp(hurr,latLoc,lonLoc);
     if maxVIn>threshold
         nSeleHurr=nSeleHurr+1;
         seleHurr.NYR=NYR(i);

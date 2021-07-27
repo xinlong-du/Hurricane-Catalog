f57104037ca661cae18aa8e11a39d500a77bb824
Xinlong Du <du.xinl@husky.neu.edu>
2021-07-27 01:46:33 -0400

    Clean the code for plotting wind field map

--- map.m
+++ map.m
@@ -1,6 +1,8 @@
 clear;clc;
 hurr10000=load('.\syntheticHurricanes\NYRSimHurV4_NE1.mat');
+hurr=hurr10000.NYRSimHur(2).SimHur(1);
 %% plot world map
+figure
 latlim = [10 70];
 lonlim = [-110 10];
 worldmap(latlim,lonlim)
@@ -9,30 +11,23 @@ plotm(coastlat,coastlon)
 geoshow(coastlat,coastlon,'color','k')
 hold on
 %% plot hurricane track
-lati1=hurr10000.NYRSimHur(2).SimHur(1).Lat;
-long1=hurr10000.NYRSimHur(2).SimHur(1).Lon;
+lati1=hurr.Lat;
+long1=hurr.Lon;
 plotm(lati1,long1,'or')
-
-% lati2=hurr10000.NYRSimHur(1).SimHur(2).Lat;
-% long2=hurr10000.NYRSimHur(1).SimHur(2).Lon;
-% plotm(lati2,long2,'or')
-% 
-% lati3=hurr10000.NYRSimHur(5).SimHur(2).Lat;
-% long3=hurr10000.NYRSimHur(5).SimHur(2).Lon;
-% plotm(lati3,long3,'or')
 %% generate grid for wind field
 i=23;
-latiGrid=lati1(i)-10:0.1:lati1(i)+10;
-longGrid=long1(i)-10:0.1:long1(i)+10;
-V=zeros(201,201);
-dir=zeros(201,201);
-alpha=zeros(201,201);
-f=zeros(201,201);
-r=zeros(201,201);
+nGrid=101;
+latiGrid=lati1(i)-5:0.1:lati1(i)+5;
+longGrid=long1(i)-5:0.1:long1(i)+5;
+V=zeros(nGrid,nGrid);
+dir=zeros(nGrid,nGrid);
+alpha=zeros(nGrid,nGrid);
+f=zeros(nGrid,nGrid);
+r=zeros(nGrid,nGrid);
 theta=hurr10000.NYRSimHur(2).SimHur(1).HeadDir;
 %% calculate parameters
-for j=1:201
-    for k=1:201
+for j=1:nGrid
+    for k=1:nGrid
         %az = azimuth(lati1(i),long1(i),latiGrid(j),longGrid(k));
         [arclen,az] = distance(lati1(i),long1(i),latiGrid(j),longGrid(k));
         alpha(j,k)=deg2rad(az-theta(i)); 
@@ -47,14 +42,14 @@ for j=1:201
         end
     end
 end
-Vt=hurr10000.NYRSimHur(2).SimHur(1).Vt_mps;
-B=hurr10000.NYRSimHur(2).SimHur(1).B;
-dP=hurr10000.NYRSimHur(2).SimHur(1).dP;
-rho=1.0;
-Rmax=hurr10000.NYRSimHur(2).SimHur(1).Rmax;
+Vt=hurr.Vt_mps;
+B=hurr.B;
+dP=hurr.dP;
+rho=1.225; %air density TBD. Georgiou(1985) used 1.0, But 1.225 is more accurate (the coutour is steeper means duration is shorter?)
+Rmax=hurr.Rmax;
 %% calculate wind field
-for j=1:201
-    for k=1:201
+for j=1:nGrid
+    for k=1:nGrid
         V(j,k)=0.5*(Vt(i)*sin(alpha(j,k))-f(j,k)*r(j,k))+sqrt(0.25*(Vt(i)*sin(alpha(j,k))-f(j,k)*r(j,k))^2+...
             B(i)*dP(i)*100/rho*(Rmax(i)/r(j,k))^B(i)*exp(-(Rmax(i)/r(j,k))^B(i)));
     end
@@ -65,13 +60,3 @@ contourf(longGrid,latiGrid,V,'ShowText','on')
 axis equal
 hold on
 quiver(longGrid,latiGrid,sin(dir),cos(dir))
-%%
-% for j=1:201
-%     for k=1:201
-%         V(j,k)=sqrt(...
-%             B(i)*dP(i)*100/rho*(Rmax(i)/r(j,k))^B(i)*exp(-(Rmax(i)/r(j,k))^B(i)));
-%     end
-% end
-% figure
-% contourf(longGrid,latiGrid,V,'ShowText','on')
-% axis equal

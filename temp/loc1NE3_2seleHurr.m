4e5d8970a5d85109fc2379b7b82d09bb73af3da1
Xinlong Du <du.xinl@husky.neu.edu>
2021-08-27 01:32:24 -0400

    Prepare 2D wind speeds

--- temp/loc1NE3_2seleHurr.m
+++ temp/loc1NE3_2seleHurr.m
@@ -2,6 +2,7 @@ clear;clc;
 load('loc1NE3-2seleHurrGood.mat');
 load('loc1NE3-2duraGood.mat');
 [maxDura,idx]=max(duraGood);
+%% 1D wind with zero padding at the end
 windRecords=zeros(maxDura/10+1,length(seleHurrGood));
 windRecords(1,:)=1:length(seleHurrGood);
 for i=1:length(seleHurrGood)
@@ -10,7 +11,7 @@ for i=1:length(seleHurrGood)
 end
 %save('windRecords.txt', windRecords, '-double', '-tab')
 dlmwrite('windRecords.txt',windRecords,'delimiter','\t')
-%%
+%% 1D wind with zero padding at the beginning and the end
 % seleHurrGood(idx)=[];
 % duraGood(idx)=[];
 % [maxDura,idx]=max(duraGood);
@@ -26,3 +27,22 @@ for i=1:length(seleHurrGood)
     end
 end
 dlmwrite('windRecords2.txt',windRecords2,'delimiter','\t')
+%% 2D wind with zero padding at the beginning and the end
+seleHurrGood(idx)=[];
+duraGood(idx)=[];
+[maxDura,idx]=max(duraGood);
+windRecords2D=zeros(maxDura/10+1,length(seleHurrGood)*2);
+windRecords2D(1,1:2:end-1)=1:length(seleHurrGood);
+windRecords2D(1,2:2:end)=1:length(seleHurrGood);
+for i=1:length(seleHurrGood)
+    numP=length(seleHurrGood{i}.VIn250N);
+    mid=round(maxDura/10/2)+1;
+    if rem(numP,2)==0
+        windRecords2D(mid-numP/2+1:mid+numP/2,2*i-1)=seleHurrGood{i}.VIn250N;
+        windRecords2D(mid-numP/2+1:mid+numP/2,2*i)=seleHurrGood{i}.VIn250E;
+    else
+        windRecords2D(mid-(numP/2-0.5):mid+(numP/2-0.5),2*i-1)=seleHurrGood{i}.VIn250N;
+        windRecords2D(mid-(numP/2-0.5):mid+(numP/2-0.5),2*i)=seleHurrGood{i}.VIn250E;
+    end
+end
+dlmwrite('windRecords2D.txt',windRecords2D,'delimiter','\t')

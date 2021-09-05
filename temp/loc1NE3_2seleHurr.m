a3f5b8478a761ba650e3211bd4e7f1c10cb11467
Xinlong Du <du.xinl@husky.neu.edu>
2021-09-05 02:38:10 -0400

    Select wind records from 4 clusters for IDA

--- temp/loc1NE3_2seleHurr.m
+++ temp/loc1NE3_2seleHurr.m
@@ -99,7 +99,7 @@ lonLoc=-69.99792;
 rad = 250; %radius, consider hurricanes within 250 km of the location
 [latC,lonC] = scircle1(latLoc,lonLoc,km2deg(rad));
 
-for j=1:length(clusters)
+for i=1:length(clusters)
 figure
 latlim = [35 45];
 lonlim = [-80 -60];
@@ -108,11 +108,133 @@ load coastlines
 plotm(coastlat,coastlon)
 geoshow(coastlat,coastlon,'color','k')
 hold on
-for i=1:length(clusters{j})
-    idxHurr=clusters{j}(i);
+for j=1:length(clusters{i})
+    idxHurr=clusters{i}(j);
     plotWind=seleHurrGood{idxHurr};
     plotm(plotWind.latIn250,plotWind.lonIn250,'r')
 end
 plotm(latC,lonC,'b')
 plotm(latLoc,lonLoc,'bo')
+end
+%% select wind records from 4 clusters for IDA analysis
+nHurrCluster=[length(clusters{1}),length(clusters{2}),length(clusters{3}),length(clusters{4})];
+nSeleHurrCluster=round(nHurrCluster/10);
+duraCluster={};
+HurrCluster={};
+duraSeleCluster=[];
+for i=1:length(clusters)
+    for j=1:length(clusters{i})
+        HurrCluster{i}{j}=seleHurrGood{clusters{i}(j)};
+        duraCluster{i}(j)=seleHurrGood{clusters{i}(j)}.dura250;
+    end
+    [dura,idx]=sort(duraCluster{i});
+    sortedHurrCluster{i}=HurrCluster{i}(idx); %sort the hurricanes by duration
+    seleHurrCluster{i}=sortedHurrCluster{i}(1:nSeleHurrCluster(i));
+    duraSeleCluster=[duraSeleCluster,dura(1:nSeleHurrCluster(i))];
+end
+%% plot sorted hurricanes (need to be cleaned with lines 96-118)
+for i=1:length(clusters)
+figure
+latlim = [35 45];
+lonlim = [-80 -60];
+worldmap(latlim,lonlim)
+load coastlines
+plotm(coastlat,coastlon)
+geoshow(coastlat,coastlon,'color','k')
+hold on
+for j=1:length(clusters{i})
+    plotWind=sortedHurrCluster{i}{j};
+    plotm(plotWind.latIn250,plotWind.lonIn250,'r')
+end
+plotm(latC,lonC,'b')
+plotm(latLoc,lonLoc,'bo')
+end
+%% plot the selected hurricanes
+figure
+histogram(duraSeleCluster/60.0,15)
+xlabel('Duration (h)')
+ylabel('Number of hurricanes')
+title('Duration considering hurricane eyes within 250 km')
+
+for i=1:length(clusters)
+figure
+latlim = [35 45];
+lonlim = [-80 -60];
+worldmap(latlim,lonlim)
+load coastlines
+plotm(coastlat,coastlon)
+geoshow(coastlat,coastlon,'color','k')
+hold on
+for j=1:nSeleHurrCluster(i)
+    plotWind=seleHurrCluster{i}{j};
+    plotm(plotWind.latIn250,plotWind.lonIn250,'r')
+end
+plotm(latC,lonC,'b')
+plotm(latLoc,lonLoc,'bo')
+end
+%%
+for i=1:length(clusters)
+    for j=1:nSeleHurrCluster(i)
+        plotWind=seleHurrCluster{i}{j};
+        figure
+        subplot(3,2,1) %whole track
+        latlim = [10 70];
+        lonlim = [-110 10];
+        worldmap(latlim,lonlim)
+        load coastlines
+        plotm(coastlat,coastlon)
+        geoshow(coastlat,coastlon,'color','k')
+        hold on
+        plotm(plotWind.latIn,plotWind.lonIn,'r')
+        plotm(latC,lonC,'b')
+    
+        subplot(3,2,3) %track within 250km
+        latlim = [35 45];
+        lonlim = [-80 -60];
+        worldmap(latlim,lonlim)
+        load coastlines
+        plotm(coastlat,coastlon)
+        geoshow(coastlat,coastlon,'color','k')
+        hold on
+        plotm(plotWind.latIn250,plotWind.lonIn250,'r')
+        plotm(latC,lonC,'b')
+        plotm(latLoc,lonLoc,'bo')
+    
+        subplot(3,2,2) %time history within 250km
+        yyaxis left
+        plot(plotWind.tIn250,plotWind.VIn250)
+        xlabel('time (min)')
+        ylabel('wind speed (m/s)')
+        ylim([0 50])
+        yyaxis right
+        plot(plotWind.tIn250,plotWind.dirIn250)
+        ylabel('wind direction (rad)')
+        ylim([0 2*pi])
+        title('Time history with in 250km (Polar)')
+    
+        subplot(3,2,4) %time history within 250km
+        yyaxis left
+        plot(plotWind.tIn250,plotWind.VIn250N)
+        xlabel('time (min)')
+        ylabel('wind speed in North (m/s)')
+        ylim([-40 40])
+        yyaxis right
+        plot(plotWind.tIn250,plotWind.VIn250E)
+        ylabel('wind speed in East (m/s)')
+        ylim([-40 40])
+        title('Time history with in 250km (Cartesian)')
+        
+        %whole time history
+        subplot(3,2,5:6)
+        yyaxis left
+        plot(10*plotWind.tIn,plotWind.VIn)
+        xlabel('time (min)')
+        ylabel('wind speed (m/s)')
+        ylim([0 50])
+        yyaxis right
+        plot(10*plotWind.tIn,plotWind.dirIn)
+        ylabel('wind direction (rad)')
+        ylim([0 2*pi])
+        title('Whole time history (Good)')
+    end
 end

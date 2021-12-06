9d20d56459707d5d93eea6d0c351f1906408cdd2
Xinlong Du <du.xinl@husky.neu.edu>
2021-12-06 02:17:08 -0500

    Plot wind records for one site
    Generate figures for the hurricane selection paper

---
+++ plotWindOneSite.m
@@ -0,0 +1,67 @@
+clear;clc;
+load('.\windRecordsMass\Grid44.mat');
+latLoc=42.7;    %Grid44
+lonLoc=-71.5;
+rad = 250; %radius, consider hurricanes within 250 km of the location
+[latC,lonC] = scircle1(latLoc,lonLoc,km2deg(rad));
+%% histogram of good duration 
+% meanDura=mean(duraGood/60.0); %convert to hours
+% figure
+% histogram(duraGood/60.0,20,'Normalization','probability')
+% xlabel('Duration (h)')
+% ylabel('Probability')
+% title('Duration considering hurricane eyes within 250 km')
+%% plot good records
+%[two peak 138 154 165 184 199 192] 
+%[one peak 136 152 191 174 171]
+%[other not typical patterns 137
+for i=1:length(seleHurrGood)
+    plotWind=seleHurrGood{i};
+    figure
+    subplot(2,2,1) %whole track
+    latlim = [10 70];
+    lonlim = [-110 10];
+    worldmap(latlim,lonlim)
+    load coastlines
+    plotm(coastlat,coastlon)
+    geoshow(coastlat,coastlon,'color','k')
+    hold on
+    plotm(plotWind.latIn,plotWind.lonIn,'r')
+    plotm(latC,lonC,'b')
+    
+    subplot(2,2,3) %track within 250km
+    latlim = [36 46];
+    lonlim = [-80 -60];
+    worldmap(latlim,lonlim)
+    load coastlines
+    plotm(coastlat,coastlon)
+    geoshow(coastlat,coastlon,'color','k')
+    hold on
+    plotm(plotWind.latIn250,plotWind.lonIn250,'r')
+    plotm(latC,lonC,'b')
+    plotm(latLoc,lonLoc,'bo')
+    
+    subplot(2,2,2) %time history within 250km
+    yyaxis left
+    plot(plotWind.tIn250,plotWind.VIn250)
+    xlabel('time (min)')
+    ylabel('wind speed (m/s)')
+    ylim([0 50])
+    yyaxis right
+    plot(plotWind.tIn250,plotWind.dirIn250)
+    ylabel('wind direction (rad)')
+    ylim([0 2*pi])
+    title('Time history with in 250km (Polar)')
+    
+    subplot(2,2,4) %time history within 250km
+    yyaxis left
+    plot(plotWind.tIn250,plotWind.VIn250N)
+    xlabel('time (min)')
+    ylabel('wind speed in North (m/s)')
+    ylim([-40 40])
+    yyaxis right
+    plot(plotWind.tIn250,plotWind.VIn250E)
+    ylabel('wind speed in East (m/s)')
+    ylim([-40 40])
+    title('Time history with in 250km (Cartesian)')
+end
